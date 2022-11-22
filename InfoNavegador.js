497c397ea0c8276b71e79803eaf567e85e93ea95
uo282944 <98974162+uo282944@users.noreply.github.com>
2022-11-23 00:45:51 +0100

    Add files via upload

---
+++ InfoNavegador.js
@@ -0,0 +1,7 @@
+infoNavegador = new Object();
+infoNavegador.nombre = navigator.appName;
+infoNavegador.idioma = navigator.language;
+infoNavegador.version = navigator.appVersion;
+infoNavegador.plataforma = navigator.platform;
+infoNavegador.vendedor = navigator.vendor;
+infoNavegador.agente = navigator.userAgent;
---
+++ MasInfoNavegador.js
@@ -0,0 +1,8 @@
+document.write("Versión: ");
+document.write(infoNavegador.version+ " ");
+document.write("Plataforma: ");
+document.write(infoNavegador.plataforma+ " ");
+document.write("Vendedor: ");
+document.write(infoNavegador.vendedor+ " ");
+document.write("Agente: ");
+document.write(infoNavegador.agente + " ");

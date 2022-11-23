860ad6c36fa58bfe83423ae6b3342f3e9ae09aa5
uo282944 <98974162+uo282944@users.noreply.github.com>
2022-11-23 01:24:56 +0100

    Delete MasInfoNavegador.js

--- MasInfoNavegador.js
+++
@@ -1,8 +0,0 @@
-document.write("Versión: ");
-document.write(infoNavegador.version+ " ");
-document.write("Plataforma: ");
-document.write(infoNavegador.plataforma+ " ");
-document.write("Vendedor: ");
-document.write(infoNavegador.vendedor+ " ");
-document.write("Agente: ");
-document.write(infoNavegador.agente + " ");

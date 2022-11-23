4f2c3c46bad06ed82cf6d588c27ffaa22f5b7e8e
uo282944 <98974162+uo282944@users.noreply.github.com>
2022-11-23 01:24:49 +0100

    Delete InfoNavegador.js

--- InfoNavegador.js
+++
@@ -1,7 +0,0 @@
-infoNavegador = new Object();
-infoNavegador.nombre = navigator.appName;
-infoNavegador.idioma = navigator.language;
-infoNavegador.version = navigator.appVersion;
-infoNavegador.plataforma = navigator.platform;
-infoNavegador.vendedor = navigator.vendor;
-infoNavegador.agente = navigator.userAgent;

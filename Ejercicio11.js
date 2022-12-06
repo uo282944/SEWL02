30f36059c58b341ce444ea59dcbcd54b6752ff99
uo282944 <98974162+uo282944@users.noreply.github.com>
2022-12-06 23:47:21 +0100

    Add files via upload

---
+++ Ejercicio11.js
@@ -0,0 +1,39 @@
+"use strict";
+class GeoLocalización {
+    constructor (){
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+    getPosicion(posicion){
+        this.longitud         = posicion.coords.longitude; 
+        this.latitud          = posicion.coords.latitude;  
+        this.precision        = posicion.coords.accuracy;
+    }
+    
+    crearElemento(tipoElemento, texto, insertarAntesDe){
+        // Crea un nuevo elemento modificando el árbol DOM
+        // El elemnto creado es de 'tipoElemento' con un 'texto' 
+        // El elemnto se coloca antes del elemnto 'insertarAntesDe'
+        var elemento = document.createElement(tipoElemento); 
+        elemento.innerHTML = texto;
+        $(insertarAntesDe).before(elemento);
+    }
+
+    cargarDatos(){
+        var datos='<h2>DATOS</h2>'; 
+        datos+='<p>Longitud: '+this.longitud +' grados</p>'; 
+        datos+='<p>Latitud: '+this.latitud +' grados</p>';
+        datos+='<p>Precisión de la latitud y longitud: '+ this.precision +' metros</p>';
+        $("section").html(datos);
+    }
+
+    verUbicacion(){
+        this.borrarContenido();
+        this.crearElemento("section","","footer");
+        this.cargarDatos();
+    }
+
+    borrarContenido(){
+        $("section").remove();
+    }
+}
+var ubicacion = new GeoLocalización();
